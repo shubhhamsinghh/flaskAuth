671c0523f702dc00553b0d90ace6c980d9d1bf78
Shubham Singh <shubham.singh@EYWA.LOCAL>
2025-07-04 16:20:21 +0530

    add login_required decorater

--- app/middleware.py
+++ app/middleware.py
@@ -1,20 +1,13 @@
 import functools
 from flask import session, redirect, url_for
+from flask_login import current_user
 
-# middleware auth
-def auth(view_func):
-    @functools.wraps(view_func)
-    def decorated(*args, **kwargs):
-        if 'user' not in session:
-            return redirect(url_for('auth.login'))
-        return view_func(*args, **kwargs)
-    return decorated
 
 # middleware guest
 def guest(view_func):
     @functools.wraps(view_func)
     def decorated(*args, **kwargs):
-        if 'user' in session:
+        if current_user.is_authenticated:
             return redirect(url_for('tasks.view_tasks'))
         return view_func(*args, **kwargs)
     return decorated
